08f0262f81023986ea863dfce974d17cf2c3750a
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-01-24 14:15:05 -0300

    Aula 20 Adicionada

---
+++ aula20/js/script.js
@@ -0,0 +1,44 @@
+function meuEscopo() {
+
+    
+    const form = document.querySelector('.form');
+
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+
+    form.addEventListener('submit', recebeEventoForm);
+
+    function recebeEventoForm (evento) {
+
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.sobrenome');
+        const altura = form.querySelector('.altura');
+
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value}, ${peso.value}, ${altura.value}</p>`
+        
+        
+        
+    };
+
+
+    
+
+    
+
+};
+meuEscopo();
