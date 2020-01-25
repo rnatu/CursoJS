e1d0f731ec9f0743ba02fee2611f0196ed24bec9
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-01-25 00:28:45 -0300

    Ajuste aula 16

--- aula16/Arrays.js
+++ aula16/Arrays.js
@@ -1,6 +1,22 @@
+function temHabilidade(skills) {
+    if (skills.indexOf("Javascript") !== -1) {
+        return console.log("Tem a skill");
+      } else {
+        console.log("Não tem a skill");
+      }
+    };
+   var skills = ["Javascript", "ReactJS", "React Native"];
+   
+   temHabilidade(skills); // true ou false
+
+   console.log(skills[0])
+
+
 //            0123456789
 //const nome = 'Aline Lima';
 //console.log(nome[0]);
+
+
 
 
 //                0        1        2      
