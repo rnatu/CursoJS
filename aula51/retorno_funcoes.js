d09f7eeaade965e550816de8a4f034d71ff524f3
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-02-12 13:59:20 -0300

    Aula 51

--- aula51/retorno_funcoes.js
+++ aula51/retorno_funcoes.js
@@ -3,8 +3,59 @@
 // Termina a função
 
 function soma(a, b) {
-    return a+b;
+    return a + b;
 }
 
-let s1 = soma(2,5);
+let s1 = soma(2, 5);
 console.log(s1);
+console.log('');
+
+
+
+///////////////////////////////////////////
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome,
+        sobrenome
+    }
+}
+
+const p1 = criaPessoa('Luiz', 'Otavio');
+const p2 = {
+    nome: 'João',
+    sobrenome: 'Oliveira'
+};
+
+console.log(p1, p2);
+console.log(typeof p1, typeof p2);
+
+
+/////////////////////////////////////////////////
+function falaFrase(comeco) {
+    function falaResto(resto) {
+        return comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+
+const fala = falaFrase('Olá'); //aqui está armazenando o retorno, que é a função falaResto (linha 38)
+const resto = fala('Mundo!.'); //aqui executa a função que foi armazenada na const fala, com os parametros, executando a função falaResto (linha 35)
+
+console.log(resto);
+
+
+function criaMultiplicador(multiplicador) {
+    //multiplicador
+    function multiplicacao(n){
+        return n * multiplicador
+    }
+    return multiplicacao;
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(4));
+console.log(triplica(4));
+console.log(quadriplica(4));
