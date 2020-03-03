70d7cfb6916fcb46b772dd7adf4d2b8d245db7aa
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-03-03 00:09:59 -0300

    Add aula 69 - forEach

---
+++ aula69/forEach.js
@@ -0,0 +1,8 @@
+// foreach somente para arrays
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+let total = 0;
+a1.forEach((valor, indice, array) => {
+    total += valor;
+});
+console.log(total);
