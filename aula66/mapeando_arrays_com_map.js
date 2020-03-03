7c370ac2413aeff34db4e5d8655c6e84e893f2e6
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-03-02 21:25:08 -0300

    Add aula 66 - Mapeando arrays com map

---
+++ aula66/mapeando_arrays_com_map.js
@@ -0,0 +1,55 @@
+// map -> Sempre tem o mesmo tamanho do array original.
+
+//               0   1   2  3  4 ...
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+console.log(numeros);
+console.log('');
+
+// * Dobre os números
+// const numerosEmDobro = numeros.map(valor => `O dobro de ${valor} é ${valor * 2}`);
+// console.log(numerosEmDobro);
+
+
+// * Para cada elemento:
+const pessoas = [
+    { nome: 'Luiz', idade: 62},     // 0
+    { nome: 'Maria', idade: 23},    // 1
+    { nome: 'Eduardo', idade: 55},  // 2
+    { nome: 'Leticia', idade: 19},  // 3
+    { nome: 'Rpsana', idade: 32},   // 4
+    { nome: 'Wallace', idade: 47},  // 5
+];
+
+
+// * Retorne apenas uma string com o nome da pessoa
+const nomeString = pessoas.map(obj => obj.nome);
+console.log(nomeString);
+console.log('');
+
+// * Remova apenas a chave "nome" do objeto
+const removeChaveNome = pessoas.map(obj => ({idade: obj.idade}));
+console.log(removeChaveNome);
+console.log('');
+
+// * Adicione uma chave de id em cada objeto
+const pessoasComId = pessoas.map((obj, indice) => {
+    const newObj = {...obj}; //copiando o objeto original (pessoas), para que não seja alterado o objeto original
+    newObj.id = indice;
+    return newObj;
+});
+console.log(pessoasComId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
