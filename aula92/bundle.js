52ff1195d53fb0fd2cebd61861f0deb886d1b82d
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-04-28 17:33:35 -0300

    Add aula 92 - Babel e can i use
    Adicionado contéudo referente a utilização do site can i use e utilização do babel juntamente com o node

---
+++ aula92/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var pessoa = function pessoa(nome, sobrenome) {
+  _classCallCheck(this, pessoa);
+
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+};
