4295bfeedbac570986b23bd8716c9db81d4d1ea5
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-05-15 17:38:43 -0300

    Add Aula 97 -  Core-js e regenerator-runtime
    Adicionado conteúdo referente a utilização do core-js e regenerator-runtime para suporte a navegadores mais antigos

---
+++ aula97/src/main.js
@@ -0,0 +1,10 @@
+// * Sempre que a aplicação precisar rodar em navegadores mais antigos.
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+
+executa();
