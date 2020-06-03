41d181938ea833f902c393f05a8e37a662e4ec33
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-06-03 16:00:20 -0300

    Node Aula 4 - File System e Recursão
    Adicionado conteúdo sobre fs (file system) e recursividade.

--- node/aula4_fs_e_recursao_mutua/index.js
+++ node/aula4_fs_e_recursao_mutua/index.js
@@ -1,23 +1,36 @@
 const fs = require('fs').promises;
 const path = require('path');
-// console.log(__filename);
 
-async function readdir(rootDir){
+async function readdir(rootDir) {
     rootDir = rootDir || path.resolve(__dirname);
     const files = await fs.readdir(rootDir);
-    walk(files);
+    walk(files, rootDir);
 }
 
-async function walk(files) {
-    for(let file of files){
-        const stats = await fs.stat();
-        console.log(file);
+async function walk(files, rootDir) {
+    for (let file of files) {
+        const fileFullPath = path.resolve(rootDir, file);
+        const stats = await fs.stat(fileFullPath);
+
+        if (/\.git/g.test(fileFullPath)) continue;
+        // * filtrando se no caminho contem .git, e se for voltar pra o inicio do laço utilizando o continue, para não exibir no console.log final
+        if (/node_modules/g.test(fileFullPath)) continue;
+        // * filtrando se no caminho contem node_modules, e se for voltar pra o inicio do laço utilizando o continue, para não exibir no console.log final
+
+        
+        
+        if (stats.isDirectory()) {
+            readdir(fileFullPath);
+            continue;
+            // * O continue retorna para a proxima iteração do laço
+        }
+        
+        if (!/\.html/g.test(fileFullPath)) continue;
+        // * filtrando se no caminho contem .html, e se não for voltar pra o inicio do laço utilizando o continue, para não exibir no console.log final
+        
+
+        console.log(fileFullPath);
     }
 }
 
-readdir('/Users/rNaTu/Documents/GitHub/CursoJS/')
-
-
-// fs.readdir(path.resolve(__dirname))
-//     .then(files => console.log(files))
-//     .catch(e => console.log(e))
+readdir('/Users/rNaTu/Documents/GitHub/CursoJS/');
