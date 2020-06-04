3a8bb0d70963c8d1f325a23162da555b7a5d1cc9
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-06-04 15:20:11 -0300

    Node Aula 5 e 6 Express & Nodemon
    Adicionado conteúdo de introdução sobre a utilização de express e nodemon

---
+++ node/aula6_express/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+// * Operações Básicas
+//       criar   ler   atualizar    apagar
+//CRUD - CREATE, READ, UPDATE,      DELETE
+//       POST    GET   PUT          DELETE
+
+
+// Rotas da aplicação
+// http://meusite.com/ <- GET -> Entregue a página /  <- raiz do site
+// http://meusite.com/sobre <- GET -> Entregue a página /sobre
+// http://meusite.com/contato <- GET -> Entregue a página /contato
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    `);
+});
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário');
+})
+
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato com a gente');
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000');
+});
