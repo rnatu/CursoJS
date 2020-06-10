38c80bc73063a96d79e51a8d1e6d58e23038ce1f
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-06-10 12:54:36 -0300

    Node Aula 11 - Arquivos estáticos
    Adicionado conteúdo referente a organização e configuração de arquivos estáticos (css, img, e arquivos da pasta public)

---
+++ node/aula11_arquivos_estaticos/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./routes');
+
+const path = require('path');
+
+// * tratando o req.body
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// * utilizando conteudo estaticos
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// * setando o view
+//setando o caminho absoluto com o path.resolve
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+//setando a engine ejs
+app.set('view engine', 'ejs');
+
+
+// * utilizando as routes
+app.use(routes);
+
+
+
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000');
+});
