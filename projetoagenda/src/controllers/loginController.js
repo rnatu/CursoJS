cbb7a8cfd29b6aa622197998f9aa58eeead2153b
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-06-26 14:23:25 -0300

    Node Projeto Agenda - Criando usuários (2)
    Finalizando a criação de usuários e utilizando hash para armazenamento das senhas no banco de dados com o bcryptjs

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -4,26 +4,27 @@ exports.index = (req, res) => {
     res.render('login');
 };
 
-exports.register =  async function(req, res) {
+
+exports.register = async function (req, res) {
     const login = new Login(req.body);
 
     try {
-    await login.register();
+        await login.register();
 
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function() {
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
+        req.session.save(function () {
             return res.redirect('back');
         });
-        return;
-    }   
-
-    req.flash('success', 'Seu usuário foi criado com sucesso.');
-    req.session.save(function() {
-        return res.redirect('back');
-    });
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         return res.render('404');
-    }   
+    }
 };
