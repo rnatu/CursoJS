134a4dcc3048a5258589bc3d9b43a4175ffa9bec
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-06-30 13:45:17 -0300

    Node Projeto Agenda - Logando o usuário
    Realizada a configuração e verificação de usuario utilizando sessions para efetuar login

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -1,14 +1,14 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if (req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 
 exports.register = async function (req, res) {
-    const login = new Login(req.body);
-
     try {
+        const login = new Login(req.body);
         await login.register();
 
         if (login.errors.length > 0) {
@@ -28,3 +28,36 @@ exports.register = async function (req, res) {
         return res.render('404');
     }
 };
+
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+
+            req.session.save(function () {
+                return res.redirect('back');
+            });
+
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('back');
+
+        });
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function (req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
--- projetoagenda/src/models/LoginModel.js
+++ projetoagenda/src/models/LoginModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcryptjs = require('bcryptjs');
 
+
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true }
@@ -19,6 +20,28 @@ class Login {
 
 
 
+
+
+    async login() {
+        this.valida();
+        if (this.errors.length > 0) return;
+
+        // # configurando user se houver email no banco de dados
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if (!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha Inválida');
+            this.user = null;
+            return;
+        }
+    }
+
+
     async register() {
         this.valida();
         if (this.errors.length > 0) return;
@@ -29,17 +52,17 @@ class Login {
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (e) {
-            console.log(e);
-        }
+        // # armazenando no user recebendo parametros criados do banco de dados (email, e senha já com hash)
+        this.user = await LoginModel.create(this.body);
     }
 
+
     async userExists() {
-        const user = await LoginModel.findOne({ email: this.body.email });
-        if (user) this.errors.push('Usuário já existe');
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if (this.user) this.errors.push('Usuário já existe');
+
     }
+
 
     valida() {
         this.cleanUp();
