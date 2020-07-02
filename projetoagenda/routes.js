a7bf05eba27afd06481d3c7a6f815a90c304c76d
Renato Xavier <40378126+rnatu@users.noreply.github.com>
2020-07-02 18:03:57 -0300

    Node Projeto Agenda - Cadastrando contatos pt1

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -3,6 +3,10 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+// middleware
+const { loginRequired } = require('./src/middlewares/middleware');
 
 
 // Rotas da home
@@ -13,5 +17,10 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de contato Com middleware
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+
 
 module.exports = route;
