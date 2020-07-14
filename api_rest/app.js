447d0decf22ada85cae2d9047b6e74265c3e627e
Renato Xavier <rnatu91@gmail.com>
2020-07-14 16:52:09 -0300

    Projeto API REST - Javascript, Node, Express e SQL
    Realizada a estruturação de pastas do projeto

--- api_rest/app.js
+++ api_rest/app.js
@@ -1,3 +1,25 @@
-export default () => {
-  console.log('olá mundo');
-};
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
