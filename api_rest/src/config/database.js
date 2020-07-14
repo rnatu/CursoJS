1efac7d6f4b44e3a1f870a8621ac3258289e31b6
Renato Xavier <rnatu91@gmail.com>
2020-07-14 18:44:51 -0300

    Projeto API REST - Configuração do Sequelize PT 1
    Configuração do sequelize PT 1

---
+++ api_rest/src/config/database.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+// Utilizar o tipo de módulo do commonJS com module.exports, devido a utilização do sequelize
+module.exports = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  dafine: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    'createdAt': 'created_at',
+    'updatedAt': 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
+};
