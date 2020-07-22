fcafa1d726d77d37168e7b28ec7e7ce6daef270f
Renato Xavier <rnatu91@gmail.com>
2020-07-22 17:03:49 -0300

    Projeto API REST - CRUD de alunos
    Foi realizada a criação do CRUD para as rodas de alunos

--- api_rest/src/controllers/AlunoController.js
+++ api_rest/src/controllers/AlunoController.js
@@ -5,6 +5,96 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno doesn't exist"],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno doesn't exist"],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno doesn't exist"],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+      return res.json(alunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
