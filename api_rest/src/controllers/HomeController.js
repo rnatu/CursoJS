462d48269a03a5dabe4228f5accbbe355c5c7669
Renato Xavier <rnatu91@gmail.com>
2020-07-22 14:01:13 -0300

    Projeto API REST - Rotas de alunos
    Foi realizada a criação da rota index para a tabela alunos, e corrigido via migrations o campo email da tabela adicionado "unique: true"

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Maria',
-      sobrenome: 'Miranda',
-      email: 'maria@email.com',
-      idade: 23,
-      peso: 57,
-      altura: 1.68,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
