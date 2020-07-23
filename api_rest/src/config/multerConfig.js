fb8f8cc3f5334830ffe72ea6938ec63ada1acce2
Renato Xavier <rnatu91@gmail.com>
2020-07-23 17:36:51 -0300

    Projeto API REST - Tratando os arquivos enviados
    Realizada a configuração no controller para filtragem do arquivo, aceitando somente jpeg e png e tratando erros

--- api_rest/src/config/multerConfig.js
+++ api_rest/src/config/multerConfig.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return callback(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
+    }
+
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads'));
