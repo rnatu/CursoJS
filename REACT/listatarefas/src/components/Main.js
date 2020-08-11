a46311909c40fc49e9f70b788194a418ba0f9fa6
Renato Xavier <rnatu91@gmail.com>
2020-08-11 11:44:47 -0300

    React (Lista de Tarefas) - Estilizando o form
    Estilizado o form da aplicação

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Main.css';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+
 export default class Main extends Component {
   // Utilizando class fields
   state = {
@@ -20,9 +23,15 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
