fa988deeafe830f830eb0d4413d88a6c6d9811ea
Renato Xavier <rnatu91@gmail.com>
2020-08-14 16:07:43 -0300

    React + Redux + Redux Saga - React Toastify
    Adicionado o React Toastify para enviar mensagens na tela

--- REACT/react-base/src/routes/index.js
+++ REACT/react-base/src/routes/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Switch } from 'react-router-dom';
 
 import MyRoute from './MyRoute';
@@ -9,7 +8,7 @@ import Page404 from '../pages/Page404';
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute exact path="/" component={Login} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
