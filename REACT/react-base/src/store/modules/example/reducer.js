73b934f1a6f664a2bc5cc1b481b135439f135efb
Renato Xavier <rnatu91@gmail.com>
2020-09-09 15:22:55 -0300

    React Base
    Finalizado o arquivo de base para React para que posse ser utilizado posteriormente

--- REACT/react-base/src/store/modules/example/reducer.js
+++ REACT/react-base/src/store/modules/example/reducer.js
@@ -7,19 +7,19 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case types.BOTAO_CLICADO_SUCCESS: {
-      console.log('Sucesso');
+      // console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
 
     case types.BOTAO_CLICADO_FAILURE: {
-      console.log('Deu erro =(');
+      // console.log('Deu erro =(');
       return state;
     }
 
     case types.BOTAO_CLICADO_REQUEST: {
-      console.log('Estou fazendo a requisição');
+      // console.log('Estou fazendo a requisição');
       return state;
     }
 
