a304e33c850ab2994c4b4222a57811cec276bc24
Renato Xavier <rnatu91@gmail.com>
2020-09-09 15:21:58 -0300

    React + Redux + Redux Saga - Redux Persist
    Realizada a configuração do redux persist para que os estados do reducer sejam mantidos no local storage

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
-import store from './store';
+import store, { persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Header />
-        <Routes />
-        <GlobalStyle />
-        <ToastContainer autoClose={3000} className="toast-container" />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} className="toast-container" />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
