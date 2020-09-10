c5723366236d2771986dc0908e2d594309fb04f3
Renato Xavier <rnatu91@gmail.com>
2020-09-10 14:38:49 -0300

    React - Consumindo a API - Configuração do arquivo
    Realizada a cópia da pasta React Base para utilização no consumo da api, servindo como uma 'base'

---
+++ REACT/consumir-api/src/pages/Login/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+import * as exempleActions from '../../store/modules/example/actions';
+
+export default function Login() {
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    dispatch(exempleActions.clicaBotaoRequest());
+  };
+
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Lorem ipsun dolor sit amet.</Paragrafo>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
+    </Container>
+  );
+}
