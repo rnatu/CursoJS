31473d65c4c407342d14e6b7639fa0b2a5cd0da1
Renato Xavier <rnatu91@gmail.com>
2020-09-11 15:43:27 -0300

    React - Consumindo a API - Criando o login #1
    Realizando a criação de usuário a partir do login PT 1

--- REACT/consumir-api/src/pages/Login/index.js
+++ REACT/consumir-api/src/pages/Login/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // ? console.log(email, password);
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('E-mail inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErrors) return;
+
+    dispatch(actions.loginRequest({ email, password }));
+  };
+
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
