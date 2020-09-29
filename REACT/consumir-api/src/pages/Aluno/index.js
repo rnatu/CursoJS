437937fb7da617d91e709f15a91d8e5e9b558603
Renato Xavier <rnatu91@gmail.com>
2020-09-29 14:49:44 -0300

    React - Consumindo a API - Exibindo a foto do aluno
    Criação da funcionalidade de exibição da foto do aluno nos menus de edição da aplicação

--- REACT/consumir-api/src/pages/Aluno/index.js
+++ REACT/consumir-api/src/pages/Aluno/index.js
@@ -4,10 +4,12 @@ import { isEmail, isInt, isFloat } from 'validator';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
+import { FaUserCircle, FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import history from '../../services/history';
 import { Container } from '../../styles/GlobalStyles';
-import { Form } from './styled';
+import { Form, ProfilePicture, Title } from './styled';
 import Loading from '../../components/Loading';
 import axios from '../../services/axios';
 import * as actions from '../../store/modules/auth/actions';
@@ -15,7 +17,7 @@ import * as actions from '../../store/modules/auth/actions';
 export default function Aluno({ match }) {
   const dispatch = useDispatch();
 
-  const id = get(match, 'params.id', 0);
+  const id = get(match, 'params.id', '');
 
   const [nome, setNome] = useState('');
   const [sobrenome, setSobrenome] = useState('');
@@ -23,6 +25,7 @@ export default function Aluno({ match }) {
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [foto, setFoto] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -33,7 +36,9 @@ export default function Aluno({ match }) {
         setIsLoading(true);
 
         const { data } = await axios.get(`/alunos/${id}`);
+
         const Foto = get(data, 'Fotos[0].url', '');
+        setFoto(Foto);
 
         setNome(data.nome);
         setSobrenome(data.sobrenome);
@@ -138,7 +143,17 @@ export default function Aluno({ match }) {
   return (
     <Container>
       <Loading isLoading={isLoading} />
-      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+
+      <Title>{id ? 'Editar aluno' : 'Novo aluno'}</Title>
+
+      {id && (
+        <ProfilePicture>
+          {foto ? <img src={foto} alt={nome} /> : <FaUserCircle size={180} />}
+          <Link to={`/fotos/${id}`}>
+            <FaEdit size={24} />
+          </Link>
+        </ProfilePicture>
+      )}
 
       <Form onSubmit={handleSubmit}>
         <input
